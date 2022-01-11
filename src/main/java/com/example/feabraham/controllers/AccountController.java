9db4b267df12d3e437a5f3887d9cae04498fa26a
Drownie <chaoscrafty01@gmail.com>
2022-01-11 23:00:27 +0700

    initial commit

---
+++ src/main/java/com/example/feabraham/controllers/AccountController.java
@@ -0,0 +1,31 @@
+package com.example.feabraham.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    /**
+     * This is a representative operation for demonstrating CSRF.
+     *
+     * To execute this method, you need to be authenticated. However, anohter site make
+     * you send request to this handler and you may not be aware of it. You may click
+     * a button, or an image so you will be the sender. The operation will be executed,
+     * because this actual application will think you are the sender and you are authenticated.
+     *
+     * @return
+     */
+    @GetMapping
+    public String transferMoney() {
+        return "account";
+    }
+
+    @PostMapping("/transfer")
+    public String transfer() {
+        return "success";
+    }
+}
---
+++ src/main/java/com/example/feabraham/feabrahamApplication.java
@@ -0,0 +1,13 @@
+package com.example.feabraham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class feabrahamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(feabrahamApplication.class, args);
+	}
+
+}
